88cf1d453e30cca6c9e4588113bc2d383a02eb60
kk <koohf0823@gmail.com>
2023-02-28 16:12:14 +0800

    upload

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@
 #define MAX_EVENT_NUMBER 10000 // 监听最大事件数量
 
 // 添加信号捕捉
-void addsig(int sig, void(handler)(int)) {
+void addsig(int sig, void(handler)(int)) 
+{
     struct sigaction sa;
     memset(&sa, '\0', sizeof(sa));
     sa.sa_handler = handler;
@@ -48,9 +49,12 @@ int main(int argc, char* argv[])
 
     // 创建线程池，初始化线程池
     threadpool<http_conn> * pool = NULL;
-    try {
+    try 
+    {
         pool = new threadpool<http_conn>;
-    } catch(...) {
+    } 
+    catch(...) 
+    {
         return 1;
     }
 
@@ -84,29 +88,35 @@ int main(int argc, char* argv[])
     http_conn::m_epollfd = epollfd;
 
     // 循环检测事件发生
-    while(1) {
+    while(1) 
+    {
         int num = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
-        if((num < 0) && (errno != EINTR)) {
+        if((num < 0) && (errno != EINTR)) 
+        {
             printf("epoll failure\n");
             break;
         } 
 
         // 循环遍历事件数组
-        for(int i = 0; i < num; i ++) {
+        for(int i = 0; i < num; i ++) 
+        {
             int sockfd = events[i].data.fd;
-            if(sockfd == listenfd) { // 有客户端连接进来
+            if(sockfd == listenfd) 
+            { // 有客户端连接进来
                 struct sockaddr_in client_address;
                 socklen_t client_addrlen = sizeof(client_address);
 
                 int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlen);
 
-                if(connfd < 0) {
+                if(connfd < 0) 
+                {
                     printf("errno is: %d\n", errno);
                     continue;
                 }
 
                 // 目前连接数满了
-                if(http_conn::m_user_count >= MAX_FD) {
+                if(http_conn::m_user_count >= MAX_FD) 
+                {
                     close(connfd);
                     continue;
                 }
@@ -114,21 +124,31 @@ int main(int argc, char* argv[])
                 // 将新的客户的数据初始化，放到数组
                 users[connfd].init(connfd, client_address);
 
-            } else if(events[i].events &(EPOLLRDHUP | EPOLLHUP | EPOLLERR)) { // 处理异常
+            } 
+            else if(events[i].events &(EPOLLRDHUP | EPOLLHUP | EPOLLERR)) 
+            { // 处理异常
                 // 对方异常断开或错误
                 users[sockfd].close_conn();
-            } else if(events[i].events & EPOLLIN) {
+            } 
+            else if(events[i].events & EPOLLIN) 
+            {
                 // 检测读行为
-                if(users[sockfd].read()) {
+                if(users[sockfd].read()) 
+                {
                     // 一次性把数据读出来
                     pool->append(users + sockfd); // 添加到线程池队列中
-                } else {
+                } 
+                else 
+                {
                     // 读失败
                     users[sockfd].close_conn();
                 }
-            } else if(events[i].events & EPOLLOUT) {
+            } 
+            else if(events[i].events & EPOLLOUT) 
+            {
                 // 检测写行为
-                if(!users[sockfd].write()) {
+                if(!users[sockfd].write()) 
+                {
                     // 可以写的时候一次性把数据写完
                     users[sockfd].close_conn();
                 }
